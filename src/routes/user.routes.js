815c9386a1173363f765a2267ea09bcac523d7d8
Gaurav Ghodinde <gauravghodinde@gmail.com>
2024-08-21 13:11:27 +0530

    fixed routing errors

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { googleUser,googleUserLogin,loginUser, registerUser } from "../controllers/user.controller.js";
+import {loginUser, registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -7,7 +7,6 @@ const router = Router();
 router.route("/auth/signup").post(registerUser);
 
 router.route("/auth/login").post(loginUser);
-router.route("/auth/google").get(googleUser);
-router.route("/auth/google/callback").get(googleUserLogin);
+
 
 export default router;
