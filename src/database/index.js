6f5194192b60ae501726acd04c5548e66a8d21fc
gauravghodinde <gauravghodinde@gmail.com>
2025-03-23 03:49:00 +0530

    test

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const uri = "mongodb+srv://gauravghodinde:dp7kw9QqSbi8cx7K@cluster0.1gg4i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://gaurav:gaurav@test.ioh5n.mongodb.net/?retryWrites=true&w=majority&appName=test";
 
 
 
